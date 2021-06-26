0cd695a1ed3a9664a10623f5a3a1f8332ad54261
Guilherme Cunha Prada <guiprada@gmail.com>
2021-06-26 18:17:35 -0300

    - Refactoring

---
+++ lightning/VM/Memory.cs
@@ -0,0 +1,82 @@
+
+using System.Collections.Generic;
+using Operand = System.UInt16;
+namespace lightning
+{
+    public class Memory<T>{
+        List<T> values;
+        List<int> markers;
+
+        public int Env{
+            get{
+                return markers.Count - 1;
+            }
+        }
+
+        public int Marker{
+            get{
+                return markers[^1];
+            }
+        }
+
+        public int Count{
+            get{
+                return values.Count;
+            }
+        }
+
+        public int Capacity{
+            get{
+                return values.Capacity;
+            }
+        }
+
+        public Memory(){
+            values = new List<T>();
+            markers = new List<int>();
+            PushEnv();
+        }
+
+        public void Trim(){
+            markers.TrimExcess();
+            values.TrimExcess();
+        }
+
+        public T Get(int index){
+            return values[index];
+        }
+
+        public T GetAt(int index, int env){
+            int this_base = markers[env];
+            return values[this_base + index];
+        }
+
+        public T GetAt(int index){
+            return values[Marker + index];
+        }
+
+        public void SetAt(T new_value, int index, int env){
+            int this_base = markers[env];
+            values[this_base + index] = new_value;
+        }
+
+        public void Set(T new_value, int index){
+            values[index] = new_value;
+        }
+
+        public int Add(T new_value){
+            values.Add(new_value);
+            return values.Count - 1;
+        }
+
+        public void PushEnv(){
+            markers.Add(values.Count);
+        }
+
+        public void PopEnv(){
+            int last_index = markers.Count -1;
+            values.RemoveRange(markers[last_index], values.Count - markers[last_index]);
+            markers.RemoveAt(last_index);
+        }
+    }
+}
---
+++ lightning/VM/Unit.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Text;
+
+using Operand = System.UInt16;
+
+#if DOUBLE
+    using Number = System.Double;
+#else
+    using Number = System.Single;
+#endif
+
+namespace lightning
+{
+    public enum UnitType{
+        Number,
+        Null,
+        Boolean,
+
+        HeapValue
+    }
+
+    public struct Unit{
+        public Number unitValue;
+        public HeapValue heapValue;
+
+        public UnitType type;
+
+        public Unit(HeapValue p_value) : this()
+        {
+            heapValue = p_value;
+            type = UnitType.HeapValue;
+        }
+        public Unit(Number p_number) : this()
+        {
+            unitValue = p_number;
+            type = UnitType.Number;
+        }
+
+        public Unit(bool p_value) : this(){
+            if(p_value == true){
+                unitValue = 1;
+            }else{
+                unitValue = 0;
+            }
+            type = UnitType.Boolean;
+        }
+
+        public Unit(String p_string) : this()
+        {
+            if(p_string == "null")
+            {
+                type = UnitType.Null;
+            }
+            else
+            {
+                throw new Exception("Trying to create a Unit with invalid string: " + p_string);
+            }
+        }
+
+        public Type HeapValueType(){
+            if(type == UnitType.HeapValue)
+                return heapValue.GetType();
+            return null;
+        }
+
+        public override string ToString()
+        {
+            if (type == UnitType.Number){
+                return unitValue.ToString();
+            }else if(type == UnitType.Null){
+                return "null";
+            }else if(type == UnitType.Boolean){
+                if(unitValue == 0)
+                    return "false";
+                if(unitValue == 1)
+                    return "true";
+                throw new Exception("Trying to get String of Invalid Boolean");
+            }else{
+                return heapValue.ToString();
+            }
+        }
+
+        public bool ToBool()
+        {
+            if (type == UnitType.Number){
+                Console.WriteLine("ERROR: Can not convert number to Bool.");
+                throw new NotImplementedException();
+            }else if(type == UnitType.Null){
+                return false;
+            }else if(type == UnitType.Boolean){
+                if(unitValue == 0)
+                    return false;
+                if(unitValue == 1)
+                    return true;
+                throw new Exception("Trying to get Value of Invalid Boolean");
+            }else{
+                return heapValue.ToBool();
+            }
+        }
+
+        public override bool Equals(object other){
+            if(other.GetType() != typeof(Unit))
+                throw new Exception("Trying to compare Unit to non Unit type");
+            if (type == UnitType.Number){
+                Type other_type = other.GetType();
+                if (((Unit)other).type == UnitType.Number){
+                    return ((Unit)other).unitValue == unitValue;
+                }
+                return false;
+            }else if(type == UnitType.Null){
+                Type other_type = other.GetType();
+                if (((Unit)other).type == UnitType.Null){
+                    return true;
+                }
+                return false;
+            }else if(type == UnitType.Boolean){
+                return ToBool() == ((Unit)other).ToBool();
+            }else{
+                return heapValue.Equals(other);
+            }
+        }
+
+        public override int GetHashCode(){
+            if (type == UnitType.Number){
+                return unitValue.GetHashCode();
+            }else if(type == UnitType.Null){
+                return UnitType.Null.GetHashCode();
+            }else if(type == UnitType.Boolean){
+                return ToBool().GetHashCode();
+            }else{
+                return heapValue.GetHashCode();
+            }
+        }
+    }
+}
