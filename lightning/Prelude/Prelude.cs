8befdd28549cf5d4d08054061ac291a5a6bfac18
Guilherme Cunha Prada <guiprada@gmail.com>
2021-07-19 12:23:10 -0300

    - Fixed reduce bug.

--- lightning/Prelude/Prelude.cs
+++ lightning/Prelude/Prelude.cs
@@ -1371,6 +1371,7 @@ namespace lightning
                 VM map_vm = vm.GetParallelVM();
 
                 Unit accumulator = new Unit(new TableUnit(null, null, null));
+                accumulator.heapUnitValue.Set(new Unit("value"), new Unit((Integer)0));
                 for(int index=0; index<table.ECount; index++){
                     List<Unit> args = new List<Unit>();
                     args.Add(new Unit(index));
