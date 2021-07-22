da2a025caf67eb5ded0369a7d96f270ea7eab6ec
Guilherme Cunha Prada <guiprada@gmail.com>
2021-07-22 13:06:23 -0300

    - Partial 3.

--- lightning/VM/UpValueMatrix.cs
+++ lightning/VM/UpValueMatrix.cs
@@ -4,9 +4,23 @@ using Operand = System.UInt16;
 namespace lightning
 {
 	public class UpValueMatrix{
+		List<Dictionary<Operand, Dictionary<Operand, UpValueUnit>>> env;
 		Dictionary<Operand, Dictionary<Operand, UpValueUnit>> values;
+
 		public UpValueMatrix(){
 			values = new Dictionary<Operand, Dictionary<Operand, UpValueUnit>>();
+			env = new List<Dictionary<Operand, Dictionary<Operand, UpValueUnit>>>();
+			env.Add(values);
+		}
+
+		public void Push(){
+			values = new Dictionary<Operand, Dictionary<Operand, UpValueUnit>>();
+			env.Add(values);
+		}
+
+		public void Pop(){
+			values = env[^1];
+			env.RemoveAt(env.Count -1);
 		}
 
 		public UpValueUnit Get(Operand x, Operand y){
@@ -30,12 +44,16 @@ namespace lightning
 		}
 
 		public void Capture(){
-
+			foreach(KeyValuePair<Operand, Dictionary<Operand, UpValueUnit>> line in values){
+				foreach(KeyValuePair<Operand, UpValueUnit> item in line.Value)
+					item.Value.Capture();
+				line.Value.Clear();
+			}
 		}
 
 		public void Clear(){
-			foreach(KeyValuePair<Operand, Dictionary<Operand, UpValueUnit>> item in values){
-				item.Value.Clear();
+			foreach(KeyValuePair<Operand, Dictionary<Operand, UpValueUnit>> line in values){
+				line.Value.Clear();
 			}
 		}
 	}
