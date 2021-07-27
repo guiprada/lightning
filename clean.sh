322bcf0d1ce509e7a0a873e2cc0149431429e31d
Guilherme Cunha Prada <guiprada@gmail.com>
2021-07-27 17:40:56 -0300

    Added clean.sh

---
+++ clean.sh
@@ -0,0 +1,15 @@
+cd lightning_programs
+	rm -f  *.ast
+	rm -f  *.tokens
+	rm -f  *.log
+	rm -f  *.chunk
+	rm -f  *.out
+
+	cd tests
+		rm -f  *.ast
+		rm -f  *.tokens
+		rm -f  *.log
+		rm -f  *.chunk
+		rm -f  *.out
+	cd ..
+cd ..
