89e0a1b06eab9b6701c282c9df87f8465443a381
Guilherme Cunha Prada <guiprada@gmail.com>
2021-08-01 03:39:20 -0300

    - Fixed performance issues.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 rm -rf linux_builds/*
 # dotnet build -c Release -o linux_builds/ lightning
-dotnet publish --nologo -r linux-x64 -c Release lightning_interpreter/lightning_interpreter.csproj
+dotnet publish --nologo -r linux-x64 -c Release -o linux_builds lightning_interpreter/lightning_interpreter.csproj
 
-mkdir -p linux_builds
-mv lightning_interpreter/bin/Release/netcoreapp5.0/linux-x64/* linux_builds/
+# mkdir -p linux_builds
+# mv lightning_interpreter/bin/Release/netcoreapp5.0/linux-x64/* linux_builds/
