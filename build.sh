151090a63ce20ad93e1e46667778cf3dcf973f1d
Guilherme Cunha Prada <guiprada@gmail.com>
2021-08-01 00:26:32 -0300

    - Improved linux build script.

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
-dotnet build -c Release -o linux_builds/ lightning
-dotnet publish -r linux-x64 -c Release -o linux_builds/ lightning_interpreter
+rm -rf linux_builds/*
+# dotnet build -c Release -o linux_builds/ lightning
+dotnet publish -r linux-x64 -c Release -o lightning_interpreter/lightning_interpreter.csproj
+
+mkdir -p linux_builds
+mv lightning_interpreter/bin/netcoreapp5.0/* linux_builds/
