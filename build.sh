a72d80308cf352c218f0bc6104f97f6650b077f9
Guilherme Cunha Prada <guiprada@gmail.com>
2021-08-01 00:29:10 -0300

    - Fixed linux build script.

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ rm -rf linux_builds/*
 dotnet publish --nologo --self-contained true -r linux-x64 -c Release /p:LinkDuringPublish=true lightning_interpreter/lightning_interpreter.csproj
 
 mkdir -p linux_builds
-mv lightning_interpreter/bin/netcoreapp5.0/* linux_builds/
+mv lightning_interpreter/bin/Release/netcoreapp5.0/* linux_builds/
