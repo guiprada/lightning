9419d2e838b635773998efb3de59150a52879c33
Guilherme Cunha Prada <guiprada@gmail.com>
2021-08-01 00:39:01 -0300

    -Fixed build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 rm -rf linux_builds/*
 # dotnet build -c Release -o linux_builds/ lightning
-dotnet publish --nologo --self-contained true -r linux-x64 -c Release /p:LinkDuringPublish=true lightning_interpreter/lightning_interpreter.csproj
+dotnet publish --nologo -r linux-x64 -c Release lightning_interpreter/lightning_interpreter.csproj
 
 mkdir -p linux_builds
-mv lightning_interpreter/bin/Release/netcoreapp5.0/* linux_builds/
+mv lightning_interpreter/bin/Release/netcoreapp5.0/linux-x64/* linux_builds/
