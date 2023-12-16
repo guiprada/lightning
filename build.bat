e51a3a21aad3a6a9dc0acda6e3bc3d9eb1ef6ceb
passolargo11 <guiprada@gmail.com>
2023-12-16 11:06:35 -0300

    - Fixed windows build script

--- build.bat
+++ build.bat
@@ -3,10 +3,10 @@ if exist win_builds rmdir win_builds /Q/S
 dotnet publish --nologo --self-contained true -r win-x64 -c Release /p:LinkDuringPublish=true lightning_interpreter\lightning_interpreter.csproj
 
 if not exist .\win_builds mkdir .\win_builds >nul
-xcopy lightning_interpreter\bin\Debug\net8.0\win-x64\* .\win_builds\ /E/H >nul
+xcopy lightning_interpreter\bin\Release\net8.0\win-x64\* .\win_builds\ /E/H >nul
 
 if not exist .\win_builds\refs mkdir .\win_builds\refs >nul
-xcopy lightning\bin\Debug\net8.0\refs\* .\win_builds\refs\ /E/H >nul
+xcopy lightning\bin\Release\net8.0\refs\* .\win_builds\refs\ /E/H >nul
 
 @REM Works but compiles old code
 @REM dotnet build -c Release -o win_builds lightning
